2516be96ecac401580553159d5d17e8249d4e0be
Ajay Singh <ajaysingh369@gmail.com>
2025-08-23 14:07:34 +0530

    Improved qualified stamp

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "runner-tracker-cache-v12"; // Increment to bust previous cache
+const CACHE_NAME = "runner-tracker-cache-v13"; // Increment to bust previous cache
 const CACHE_LIFETIME = 30 * 60 * 1000; // 30 mins
 
 const urlsToCache = [
